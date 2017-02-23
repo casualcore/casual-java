d3df7ccf3bb5c211384d03fd19cf6b0e46777f59
Jon-Erik Johnzon (TorrentKatten) <jone@torrentkatten.se>
2017-02-23 10:43:27 +0100

    ironjcamar jca 1.7 skeleton and gradle build files

---
+++ casual/casual-jca/src/main/java/se/kodarkatten/casual/CasualConnectionImpl.java
@@ -0,0 +1,83 @@
+/*
+ * IronJacamar, a Java EE Connector Architecture implementation
+ * Copyright 2013, Red Hat Inc, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package se.kodarkatten.casual;
+
+import java.util.logging.Logger;
+
+/**
+ * CasualConnectionImpl
+ *
+ * @version $Revision: $
+ */
+public class CasualConnectionImpl implements CasualConnection
+{
+   /** The logger */
+   private static Logger log = Logger.getLogger(CasualConnectionImpl.class.getName());
+
+   /** ManagedConnection */
+   private CasualManagedConnection mc;
+
+   /** ManagedConnectionFactory */
+   private CasualManagedConnectionFactory mcf;
+
+   /**
+    * Default constructor
+    * @param mc CasualManagedConnection
+    * @param mcf CasualManagedConnectionFactory
+    */
+   public CasualConnectionImpl(CasualManagedConnection mc, CasualManagedConnectionFactory mcf)
+   {
+      this.mc = mc;
+      this.mcf = mcf;
+   }
+
+   /**
+    * Call me
+    */
+   public void callMe()
+   {
+      if (mc != null)
+         mc.callMe();
+   }
+
+   /**
+    * Close
+    */
+   public void close()
+   {
+      if (mc != null)
+      {
+         mc.closeHandle(this);
+         mc = null;
+      }
+
+   }
+
+   /**
+    * Set ManagedConnection
+    */
+   void setManagedConnection(CasualManagedConnection mc)
+   {
+      this.mc = mc;
+   }
+
+}
---
+++ casual/casual-jca/src/main/java/se/kodarkatten/casual/mbean/CasualMBeanImpl.java
@@ -0,0 +1,112 @@
+/*
+ * IronJacamar, a Java EE Connector Architecture implementation
+ * Copyright 2013, Red Hat Inc, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package se.kodarkatten.casual.mbean;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.naming.InitialContext;
+
+import se.kodarkatten.casual.CasualConnection;
+import se.kodarkatten.casual.CasualConnectionFactory;
+
+/**
+ * CasualMBeanImpl
+ *
+ * @version $Revision: $
+ */
+public class CasualMBeanImpl implements CasualMBean
+{
+   /** JNDI name */
+   private static final String JNDI_NAME = "java:/eis/Casual";
+
+   /** MBeanServer instance */
+   private MBeanServer mbeanServer;
+
+   /** Object Name */
+   private String objectName;
+
+   /** The actual ObjectName instance */
+   private ObjectName on;
+
+   /** Registered */
+   private boolean registered;
+
+
+   /**
+    * Set the MBean server
+    * @param v The value
+    */
+   public void setMBeanServer(MBeanServer v)
+   {
+      mbeanServer = v;
+   }
+
+   /**
+    * Start
+    * @exception Throwable Thrown in case of an error
+    */
+   public void start() throws Throwable
+   {
+      if (mbeanServer == null)
+         throw new IllegalArgumentException("MBeanServer is null");
+      on = new ObjectName(mbeanServer.getDefaultDomain() + objectName);
+      mbeanServer.registerMBean(this, on);
+      registered = true;
+   }
+
+   /**
+    * Stop
+    * @exception Throwable Thrown in case of an error
+    */
+   public void stop() throws Throwable
+   {
+      if (registered)
+         mbeanServer.unregisterMBean(on); 
+   }
+
+   /**
+    * Call me
+    * @throws Exception exception
+    */
+   @Override
+   public void callMe() throws Exception
+   {
+      getConnection().callMe();
+   }
+
+   /**
+    * GetConnection
+    * @return CasualConnection
+    */
+   private CasualConnection getConnection() throws Exception
+   {
+      InitialContext context = new InitialContext();
+      CasualConnectionFactory factory = (CasualConnectionFactory)context.lookup(JNDI_NAME);
+      CasualConnection conn = factory.getConnection();
+      if (conn == null)
+      {
+         throw new RuntimeException("No connection");
+      }
+      return conn;
+   }
+
+}
