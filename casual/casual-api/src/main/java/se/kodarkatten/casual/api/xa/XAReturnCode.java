2f6f77401528d90d2ff90725651905508a0d63d4
Mathias Creutz <mathiascreutz@gmail.com>
2017-04-21 07:52:19 +0000

    Merged in feature/valuesFromXAException (pull request #9)
    use values from XAException
    
    Approved-by: Jon-Erik Johnzon <jone@torrentkatten.se>

--- casual/casual-api/src/main/java/se/kodarkatten/casual/api/xa/XAReturnCode.java
+++ casual/casual-api/src/main/java/se/kodarkatten/casual/api/xa/XAReturnCode.java
@@ -1,36 +1,37 @@
 package se.kodarkatten.casual.api.xa;
 
+import javax.transaction.xa.XAException;
+
 /**
  * Created by aleph on 2017-03-30.
  */
 public enum XAReturnCode
 {
-    XA_RBBASE(100),
-    XA_RBROLLBACK(XA_RBBASE.id),
-    XA_RBCOMMFAIL(XA_RBBASE.id + 1),
-    XA_RBDEADLOCK(XA_RBBASE.id + 2),
-    XA_RBINTEGRITY(XA_RBBASE.id + 3),
-    XA_RBOTHER(XA_RBBASE.id + 4),
-    XA_RBPROTO(XA_RBBASE.id + 5),
-    XA_RBTIMEOUT(XA_RBBASE.id + 6),
-    XA_RBTRANSIENT(XA_RBBASE.id + 6),
-    XA_RBEND(XA_RBTRANSIENT.id),
-    XA_NOMIGRATE(9),
-    XA_HEURHAZ(8),
-    XA_HEURCOM(7),
-    XA_HEURRB(6),
-    XA_HEURMIX(5),
-    XA_RETRY(4),
-    XA_RDONLY(3),
+    XA_RBROLLBACK(XAException.XA_RBROLLBACK),
+    XA_RBCOMMFAIL(XAException.XA_RBCOMMFAIL),
+    XA_RBDEADLOCK(XAException.XA_RBDEADLOCK),
+    XA_RBINTEGRITY(XAException.XA_RBINTEGRITY),
+    XA_RBOTHER(XAException.XA_RBOTHER),
+    XA_RBPROTO(XAException.XA_RBPROTO),
+    XA_RBTIMEOUT(XAException.XA_RBTIMEOUT),
+    XA_RBTRANSIENT(XAException.XA_RBTRANSIENT),
+    XA_RBEND(XAException.XA_RBEND),
+    XA_NOMIGRATE(XAException.XA_NOMIGRATE),
+    XA_HEURHAZ(XAException.XA_HEURHAZ),
+    XA_HEURCOM(XAException.XA_HEURCOM),
+    XA_HEURRB(XAException.XA_HEURRB),
+    XA_HEURMIX(XAException.XA_HEURMIX),
+    XA_RETRY(XAException.XA_RETRY),
+    XA_RDONLY(XAException.XA_RDONLY),
     XA_OK(0),
-    XAER_ASYNC(-2),
-    XAER_RMERR(-3),
-    XAER_NOTA(-4),
-    XAER_INVAL(-5),
-    XAER_PROTO(-6),
-    XAER_RMFAIL(-7),
-    XAER_DUPID(-8),
-    XAER_OUTSIDE(-9);
+    XAER_ASYNC(XAException.XAER_ASYNC),
+    XAER_RMERR(XAException.XAER_RMERR),
+    XAER_NOTA(XAException.XAER_NOTA),
+    XAER_INVAL(XAException.XAER_INVAL),
+    XAER_PROTO(XAException.XAER_PROTO),
+    XAER_RMFAIL(XAException.XAER_RMFAIL),
+    XAER_DUPID(XAException.XAER_DUPID),
+    XAER_OUTSIDE(XAException.XAER_OUTSIDE);
     private int id;
     XAReturnCode(int id)
     {
