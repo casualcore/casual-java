522cbdc8ce72256542e14c9168f5efd3f07df164
Mathias Creutz <mathiascreutz@yahoo.se>
2022-05-05 16:17:42 +0200

    licence

--- casual/casual-api/src/main/java/se/laz/casual/api/queue/QueueDetails.java
+++ casual/casual-api/src/main/java/se/laz/casual/api/queue/QueueDetails.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
 package se.laz.casual.api.queue;
 
 import java.util.Objects;
