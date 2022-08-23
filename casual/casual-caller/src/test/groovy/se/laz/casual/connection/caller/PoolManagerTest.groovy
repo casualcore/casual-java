d7e497286d45b6a617780a64531e0da616cc3900
Mathias Creutz <mathiascreutz@yahoo.se>
2022-08-23 15:44:22 +0200

    intellij mishap

---
+++ casual/casual-caller/src/test/groovy/se/laz/casual/connection/caller/PoolManagerTest.groovy
@@ -0,0 +1,7 @@
+package se.laz.casual.connection.caller
+
+class PoolManagerTest
+{
+   // PoolManager(ConnectionFactoryEntryStore connectionFactoryEntryStore, TransactionLess.PoolDataRetriever poolDataRetriever, Event<NewDomain> newDomain, Event<DomainGone> domainGone)
+
+}
