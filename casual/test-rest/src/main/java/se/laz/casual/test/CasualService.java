98dccab9bc8d3329b88248f049ae20abaf4e0fb8
Mathias Creutz <mathiascreutz@yahoo.se>
2022-08-29 11:20:03 +0200

    cleanup, version bump

--- casual/test-rest/src/main/java/se/laz/casual/test/CasualService.java
+++ casual/test-rest/src/main/java/se/laz/casual/test/CasualService.java
@@ -10,9 +10,16 @@ import se.laz.casual.api.buffer.CasualBuffer;
 import se.laz.casual.api.buffer.ServiceReturn;
 import se.laz.casual.api.buffer.type.OctetBuffer;
 import se.laz.casual.api.flags.AtmiFlags;
+import se.laz.casual.api.flags.ErrorState;
 import se.laz.casual.api.flags.Flag;
 import se.laz.casual.api.flags.ServiceReturnState;
+import se.laz.casual.api.queue.DequeueReturn;
+import se.laz.casual.api.queue.EnqueueReturn;
+import se.laz.casual.api.queue.MessageSelector;
+import se.laz.casual.api.queue.QueueInfo;
+import se.laz.casual.api.queue.QueueMessage;
 import se.laz.casual.connection.caller.CasualCaller;
+import se.laz.casual.test.service.remote.QueueCallFailedException;
 import se.laz.casual.test.service.remote.ServiceCallFailedException;
 
 import javax.annotation.Resource;
@@ -40,14 +47,14 @@ public class CasualService
     @POST
     @Consumes("application/casual-x-octet")
     @Path("{serviceName}")
-    public Response echoRequest(@PathParam("serviceName") String serviceName, InputStream inputStream)
+    public Response serviceRequest(@PathParam("serviceName") String serviceName, InputStream inputStream)
     {
         try
         {
             byte[] data = IOUtils.toByteArray(inputStream);
             Flag<AtmiFlags> flags = Flag.of(AtmiFlags.NOFLAG);
             OctetBuffer buffer = OctetBuffer.of(data);
-            return Response.ok().entity(makeCasualCall(buffer, serviceName, flags).getBytes().get(0)).build();
+            return Response.ok().entity(makeServiceCall(buffer, serviceName, flags).getBytes().get(0)).build();
         }
         catch (Exception e)
         {
@@ -61,7 +68,54 @@ public class CasualService
             return Response.serverError().entity(sw.toString()).build();
         }
     }
-    private CasualBuffer makeCasualCall(CasualBuffer msg, String serviceName, Flag<AtmiFlags> flags)
+
+    @POST
+    @Consumes("application/casual-x-octet")
+    @Path("enqueue/{queueName}")
+    public Response enqueue(@PathParam("queueName") String queueName, InputStream inputStream)
+    {
+        try
+        {
+            byte[] data = IOUtils.toByteArray(inputStream);
+            OctetBuffer buffer = OctetBuffer.of(data);
+            return Response.ok().entity(makeEnqueueCall(buffer, queueName)).build();
+        }
+        catch (Exception e)
+        {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            if (!ctx.getRollbackOnly())
+            {
+                ctx.setRollbackOnly();
+            }
+            return Response.serverError().entity(sw.toString()).build();
+        }
+    }
+
+    @POST
+    @Consumes("application/casual-x-octet")
+    @Path("dequeue/{queueName}")
+    public Response dequeue(@PathParam("queueName") String queueName)
+    {
+        try
+        {
+            return Response.ok().entity(makeDequeueCall(queueName)).build();
+        }
+        catch (Exception e)
+        {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            if (!ctx.getRollbackOnly())
+            {
+                ctx.setRollbackOnly();
+            }
+            return Response.serverError().entity(sw.toString()).build();
+        }
+    }
+
+    private CasualBuffer makeServiceCall(CasualBuffer msg, String serviceName, Flag<AtmiFlags> flags)
     {
         ServiceReturn<CasualBuffer> reply = casualCaller.tpcall(serviceName, msg, flags);
         if(reply.getServiceReturnState() == ServiceReturnState.TPSUCCESS)
@@ -71,5 +125,24 @@ public class CasualService
         throw new ServiceCallFailedException("tpcall failed: " + reply.getErrorState());
     }
 
+    private EnqueueReturn makeEnqueueCall(CasualBuffer msg, String queueName)
+    {
+        EnqueueReturn reply = casualCaller.enqueue(QueueInfo.of(queueName), QueueMessage.of(msg));
+        if(reply.getErrorState() == ErrorState.OK)
+        {
+            return reply;
+        }
+        throw new QueueCallFailedException("enqueue for queue: " + queueName + " failed with: " + reply.getErrorState());
+    }
+
+    private QueueMessage makeDequeueCall(String queueName)
+    {
+        DequeueReturn reply = casualCaller.dequeue(QueueInfo.of(queueName), MessageSelector.of());
+        if(reply.getErrorState() == ErrorState.OK)
+        {
+            return reply.getQueueMessage().orElseThrow(() -> new QueueCallFailedException("dequeue: " + queueName + " errorstate ok but missing message!"));
+        }
+        throw new QueueCallFailedException("dequeue for queue: " + queueName + " failed with: " + reply.getErrorState());
+    }
 
 }
