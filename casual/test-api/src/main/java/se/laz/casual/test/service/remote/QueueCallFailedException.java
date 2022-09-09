2142b9d9032ee5ac0de4b385759dd62af21ac7ea
Mathias Creutz <mathiascreutz@yahoo.se>
2022-09-09 15:21:31 +0200

    copyleft

--- casual/test-api/src/main/java/se/laz/casual/test/service/remote/QueueCallFailedException.java
+++ casual/test-api/src/main/java/se/laz/casual/test/service/remote/QueueCallFailedException.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
 package se.laz.casual.test.service.remote;
 
 public class QueueCallFailedException extends RuntimeException
