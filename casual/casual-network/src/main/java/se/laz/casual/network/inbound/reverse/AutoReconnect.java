8bae4fd9e1bff35d6c86db32bbc57ee8d79306ea
Mathias Creutz <mathiascreutz@yahoo.se>
2022-09-19 14:04:06 +0200

    refactoring

--- casual/casual-network/src/main/java/se/laz/casual/network/inbound/reverse/AutoReconnect.java
+++ casual/casual-network/src/main/java/se/laz/casual/network/inbound/reverse/AutoReconnect.java
@@ -6,50 +6,41 @@
 
 package se.laz.casual.network.inbound.reverse;
 
-import se.laz.casual.network.JEEConcurrencyFactory;
 import se.laz.casual.network.reverse.inbound.ReverseInboundListener;
 import se.laz.casual.network.reverse.inbound.ReverseInboundServer;
 
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public class AutoReconnect implements Runnable
+public class AutoReconnect
 {
-    private final ReverseInboundConnectionInformation reverseInboundConnectionInformation;
-    private final ReverseInboundListener eventListener;
-    private final StaggeredOptions staggeredOptions;
-
+    private final RepeatUntilSuccessTask<ReverseInboundServer> task;
     private AutoReconnect(ReverseInboundConnectionInformation reverseInboundConnectionInformation,
                           ReverseInboundListener eventListener,
                           StaggeredOptions staggeredOptions)
     {
-        this.reverseInboundConnectionInformation = reverseInboundConnectionInformation;
-        this.eventListener = eventListener;
-        this.staggeredOptions = staggeredOptions;
-        JEEConcurrencyFactory.getManagedScheduledExecutorService().schedule(this, staggeredOptions.getNext().toMillis(), TimeUnit.MILLISECONDS);
+       Supplier<ReverseInboundServer> supplier = () -> ReverseInboundServerImpl.of(reverseInboundConnectionInformation, eventListener);
+       Consumer<ReverseInboundServer> consumer = eventListener::connected;
+       this.task = RepeatUntilSuccessTask.of(supplier, consumer, staggeredOptions);
+       this.task.start();
     }
 
     public static AutoReconnect of(ReverseInboundConnectionInformation reverseInboundConnectionInformation,
                           ReverseInboundListener eventListener,
                           StaggeredOptions staggeredOptions)
     {
-        Objects.requireNonNull(reverseInboundConnectionInformation,"reverseInboundConnectionInformation can not be null");
-        Objects.requireNonNull(eventListener, "eventListener can not be null");
-        Objects.requireNonNull(staggeredOptions, "staggeredOptions can not be null");
-        return new AutoReconnect(reverseInboundConnectionInformation, eventListener, staggeredOptions);
+       Objects.requireNonNull(reverseInboundConnectionInformation,"reverseInboundConnectionInformation can not be null");
+       Objects.requireNonNull(eventListener, "eventListener can not be null");
+       Objects.requireNonNull(staggeredOptions, "staggeredOptions can not be null");
+       return new AutoReconnect(reverseInboundConnectionInformation, eventListener, staggeredOptions);
     }
 
-    @Override
-    public void run()
-    {
-        try
-        {
-            ReverseInboundServer server = ReverseInboundServerImpl.of(reverseInboundConnectionInformation, eventListener);
-            eventListener.connected(server);
-        }
-        catch(Exception e)
-        {
-            JEEConcurrencyFactory.getManagedScheduledExecutorService().schedule(this, staggeredOptions.getNext().toMillis(), TimeUnit.MILLISECONDS);
-        }
-    }
+   @Override
+   public String toString()
+   {
+      return "AutoReconnect{" +
+              "task=" + task +
+              '}';
+   }
 }
---
+++ casual/casual-network/src/main/java/se/laz/casual/network/inbound/reverse/RepeatUntilSuccessTask.java
@@ -0,0 +1,57 @@
+package se.laz.casual.network.inbound.reverse;
+
+import se.laz.casual.network.JEEConcurrencyFactory;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class RepeatUntilSuccessTask<T> implements Runnable
+{
+   private final Supplier<T> supplier;
+   private final Consumer<T> consumer;
+   private final StaggeredOptions staggeredOptions;
+
+   private RepeatUntilSuccessTask(Supplier<T> supplier, Consumer<T> consumer, StaggeredOptions staggeredOptions)
+   {
+      this.supplier = supplier;
+      this.consumer = consumer;
+      this.staggeredOptions = staggeredOptions;
+   }
+
+   public static <T> RepeatUntilSuccessTask<T> of(Supplier<T> supplier, Consumer<T> consumer, StaggeredOptions staggeredOptions)
+   {
+      Objects.requireNonNull(supplier, "supplier can not be null");
+      Objects.requireNonNull(consumer, "consumer can not be null");
+      return  new RepeatUntilSuccessTask<>(supplier, consumer, staggeredOptions);
+   }
+
+   public void start()
+   {
+      JEEConcurrencyFactory.getManagedScheduledExecutorService().schedule(this, staggeredOptions.getNext().toMillis(), TimeUnit.MILLISECONDS);
+   }
+
+   @Override
+   public void run()
+   {
+      try
+      {
+         consumer.accept(supplier.get());
+      }
+      catch(Exception e)
+      {
+         JEEConcurrencyFactory.getManagedScheduledExecutorService().schedule(this, staggeredOptions.getNext().toMillis(), TimeUnit.MILLISECONDS);
+      }
+   }
+
+   @Override
+   public String toString()
+   {
+      return "RepeatUntilSuccessTask{" +
+              "supplier=" + supplier +
+              ", consumer=" + consumer +
+              ", staggeredOptions=" + staggeredOptions +
+              '}';
+   }
+}
