44506562addf789b5d6f5a2b0f6c23ad18483f3d
Mathias Creutz <mathiascreutz@yahoo.se>
2022-10-24 15:16:18 +0200

    tests

---
+++ casual/casual-api/src/test/groovy/se/laz/casual/api/util/work/RepeatUntilSuccessTaskWorkTest.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+package se.laz.casual.api.util.work
+
+import spock.lang.Specification
+
+import javax.resource.spi.work.WorkException
+import javax.resource.spi.work.WorkManager
+import java.util.function.Consumer
+import java.util.function.Supplier
+
+class RepeatUntilSuccessTaskWorkTest extends Specification
+{
+   def 'will retry once and then fail with CasualWorkException'()
+   {
+      given:
+      def payload = 'hello'
+      Supplier<String> supplier = {payload}
+      Consumer<String> consumer = {it ->
+         assert it == payload
+      }
+      WorkManager workManager = Mock(WorkManager){
+         2 * scheduleWork( _ as RepeatUntilSuccessTaskWork, WorkManager.INDEFINITE, null, _ as RepeatUntilSuccessTaskWorkListener) >> {
+            throw new WorkException("Due to some internal reason")}
+      }
+      Supplier<WorkManager> workManagerSupplier = {
+         workManager
+      }
+      when:
+      RepeatUntilSuccessTaskWork taskWork = RepeatUntilSuccessTaskWork.of(supplier, consumer, workManagerSupplier)
+      taskWork.start()
+      then:
+      thrown(CasualWorkException)
+   }
+
+   def 'No work exception, only scheduled once'()
+   {
+      given:
+      def payload = 'hello'
+      Supplier<String> supplier = {payload}
+      Consumer<String> consumer = {it ->
+         assert it == payload
+      }
+      WorkManager workManager = Mock(WorkManager) {
+         1 * scheduleWork(_ as RepeatUntilSuccessTaskWork, WorkManager.INDEFINITE, null, _ as RepeatUntilSuccessTaskWorkListener)
+      }
+      Supplier<WorkManager> workManagerSupplier = {
+         workManager
+      }
+      when:
+      RepeatUntilSuccessTaskWork taskWork = RepeatUntilSuccessTaskWork.of(supplier, consumer, workManagerSupplier)
+      taskWork.start()
+      then:
+      noExceptionThrown()
+   }
+}
