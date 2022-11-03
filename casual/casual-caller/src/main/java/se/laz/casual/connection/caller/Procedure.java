fe0a9315f13d2f56e65a0c400d8d58bd39a1ebfe
Mathias Creutz <mathiascreutz@yahoo.se>
2022-11-03 13:48:10 +0100

    copyleft, version

--- casual/casual-caller/src/main/java/se/laz/casual/connection/caller/Procedure.java
+++ casual/casual-caller/src/main/java/se/laz/casual/connection/caller/Procedure.java
@@ -1,5 +1,13 @@
+/*
+ * Copyright (c) 2022, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
 package se.laz.casual.connection.caller;
 
+/**
+ * No arg(s), no return, side effect only - procedure
+ */
 @FunctionalInterface
 public interface Procedure
 {
