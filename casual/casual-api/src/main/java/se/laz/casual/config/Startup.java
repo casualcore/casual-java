656d5dc304c77d064871fa2692bb7744e61f20ef
Mathias Creutz <mathiascreutz@yahoo.se>
2023-02-07 15:33:09 +0100

    import

--- casual/casual-api/src/main/java/se/laz/casual/config/Startup.java
+++ casual/casual-api/src/main/java/se/laz/casual/config/Startup.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 public class Startup
