f513209baceeaced52fe540ceb49fb7fd34ad1ab
Mathias Creutz <mathiascreutz@yahoo.se>
2023-10-24 16:24:07 +0200

    test, copyleft

--- casual/casual-network/src/test/groovy/se/laz/casual/network/outbound/NettyNetworkConnectionTest.groovy
+++ casual/casual-network/src/test/groovy/se/laz/casual/network/outbound/NettyNetworkConnectionTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2018, The casual project. All rights reserved.
+ * Copyright (c) 2017 - 2023, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
