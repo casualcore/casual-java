cdcbb19a131816ab73283627da0c31ca206221b6
Mathias Creutz <mathiascreutz@yahoo.se>
2024-01-19 15:27:55 +0100

    Feature/3.2/service call event (#85)
    Service call event, it will be used for service call metrics

---
+++ casual/casual-event/src/main/java/se/laz/casual/event/ServiceCallEventImpl.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2024, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+package se.laz.casual.event;
+
+import javax.transaction.xa.Xid;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ServiceCallEventImpl implements ServiceCallEvent
+{
+    private final String service;
+    private final String parent;
+    private final int processId;
+    private final UUID execution;
+    private final Xid transactionId;
+    private final long start;
+    private final long end;
+    private final long pending;
+    private final int code;
+    private final Order order;
+
+    private ServiceCallEventImpl(Builder builder)
+    {
+        service = builder.service;
+        parent = builder.parent;
+        processId = builder.processId;
+        execution = builder.execution;
+        transactionId = builder.transactionId;
+        start = builder.start;
+        end = builder.end;
+        pending = builder.pending;
+        code = builder.code;
+        order = builder.order;
+    }
+
+    @Override
+    public String getService()
+    {
+        return service;
+    }
+
+    @Override
+    public Optional<String> getParent()
+    {
+        return Optional.of(parent);
+    }
+
+    @Override
+    public int getProcessId()
+    {
+        return processId;
+    }
+
+    @Override
+    public UUID getExecution()
+    {
+        return execution;
+    }
+
+    @Override
+    public Xid getTransactionId()
+    {
+        return transactionId;
+    }
+
+    @Override
+    public long getStart()
+    {
+        return start;
+    }
+
+    @Override
+    public long getEnd()
+    {
+        return end;
+    }
+
+    @Override
+    public long getPending()
+    {
+        return pending;
+    }
+
+    @Override
+    public int getCode()
+    {
+        return code;
+    }
+
+    @Override
+    public Order getOrder()
+    {
+        return order;
+    }
+
+    public static Builder createBuilder()
+    {
+        return new Builder();
+    }
+
+    public static final class Builder
+    {
+        private String service;
+        private String parent;
+        private int processId;
+        private UUID execution;
+        private Xid transactionId;
+        private long start;
+        private long end;
+        private long pending;
+        private int code;
+        private Order order;
+
+        public Builder withService(String service)
+        {
+            this.service = service;
+            return this;
+        }
+
+        public Builder withParent(String parent)
+        {
+            this.parent = parent;
+            return this;
+        }
+
+        public Builder withPid(int processId)
+        {
+            this.processId = processId;
+            return this;
+        }
+
+        public Builder withExecution(UUID execution)
+        {
+            this.execution = execution;
+            return this;
+        }
+
+        public Builder withTransactionId(Xid transactionId)
+        {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder withStart(long start)
+        {
+            this.start = start;
+            return this;
+        }
+
+        public Builder withEnd(long end)
+        {
+            this.end = end;
+            return this;
+        }
+
+        public Builder withPending(long pending)
+        {
+            this.pending = pending;
+            return this;
+        }
+
+        public Builder withCode(int code)
+        {
+            this.code = code;
+            return this;
+        }
+
+        public Builder withOrder(Order order)
+        {
+            this.order = order;
+            return this;
+        }
+
+        public ServiceCallEventImpl build()
+        {
+            return new ServiceCallEventImpl(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        ServiceCallEventImpl that = (ServiceCallEventImpl) o;
+        return getProcessId() == that.getProcessId() && getStart() == that.getStart() && getEnd() == that.getEnd() && getPending() == that.getPending() && getCode() == that.getCode() && Objects.equals(getService(), that.getService()) && Objects.equals(getParent(), that.getParent()) && Objects.equals(getExecution(), that.getExecution()) && Objects.equals(getTransactionId(), that.getTransactionId()) && getOrder() == that.getOrder();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(getService(), getParent(), getProcessId(), getExecution(), getTransactionId(), getStart(), getEnd(), getPending(), getCode(), getOrder());
+    }
+}
---
+++ casual/casual-event/src/test/groovy/se/laz/casual/event/ServiceCallEventImplTest.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
+import se.laz.casual.api.flags.ErrorState
+import se.laz.casual.event.Order
+import se.laz.casual.event.ServiceCallEventImpl
+import spock.lang.Specification
+
+import javax.transaction.xa.Xid
+
+class ServiceCallEventImplTest extends Specification
+{
+   def firstService = 'Service One'
+   def secondService = 'Service Two'
+   def firstParent = 'Elvira'
+   def secondParent = 'Elvis'
+   def firstExecution = UUID.randomUUID()
+   def secondExecution = UUID.randomUUID()
+   def firstPid = 12345
+   def secondPid = 123456
+   def firstTransactionId = Mock(Xid)
+   def secondTransactionId = Mock(Xid)
+   def firstStart = 1
+   def secondStart = 43
+   def firstEnd = 42
+   def secondEnd = 128
+   def firstPending = 0
+   def secondPending = 14
+   def firstCode = ErrorState.OK.value
+   def secondCode = ErrorState.TPENOENT.value
+   def firstOrder = Order.SEQUENTIAL
+   def secondOrder = Order.CONCURRENT
+    
+   def 'identity'()
+   {
+      given:
+
+      when:
+      ServiceCallEventImpl firstEntry = createEntry([service: firstService, parent: firstParent, pid: firstPid, execution: firstExecution, transactionId: firstTransactionId, start: firstStart, end: firstEnd, pending: firstPending, code: firstCode, order: firstOrder])
+      ServiceCallEventImpl secondEntry = createEntry([service: secondService, parent: secondParent, pid: secondPid, execution: secondExecution, transactionId: secondTransactionId, start: secondStart, end: secondEnd, pending: secondPending, code: secondCode, order: secondOrder])
+      then:
+      firstEntry == firstEntry
+      firstEntry != secondEntry
+
+      firstEntry.hashCode() == firstEntry.hashCode()
+      firstEntry.hashCode() != secondEntry.hashCode()
+
+      firstEntry.getService() == firstService
+      firstEntry.getParent().get() == firstParent
+      firstEntry.getProcessId() == firstPid
+      firstEntry.getExecution() == firstExecution
+      firstEntry.getTransactionId() == firstTransactionId
+      firstEntry.getStart() == firstStart
+      firstEntry.getEnd() == firstEnd
+      firstEntry.getPending() == firstPending
+      firstEntry.getCode() == firstCode
+      firstEntry.getOrder() == firstOrder
+
+      secondEntry.getService() == secondService
+      secondEntry.getParent().get() == secondParent
+      secondEntry.getProcessId() == secondPid
+      secondEntry.getExecution() == secondExecution
+      secondEntry.getTransactionId() == secondTransactionId
+      secondEntry.getStart() == secondStart
+      secondEntry.getEnd() == secondEnd
+      secondEntry.getPending() == secondPending
+      secondEntry.getCode() == secondCode
+      secondEntry.getOrder() == secondOrder
+   }
+
+   def createEntry(data)
+   {
+      return ServiceCallEventImpl.createBuilder()
+              .withService(data.service)
+              .withParent(data.parent)
+              .withPid(data.pid)
+              .withExecution(data.execution)
+              .withTransactionId(data.transactionId)
+              .withStart(data.start)
+              .withEnd(data.end)
+              .withPending(data.pending)
+              .withCode(data.code)
+              .withOrder(data.order)
+              .build()
+   }
+}
