4629f43f9b8cd99b1e2b85361eef4c16743068ea
Mathias Creutz <mathiascreutz@yahoo.se>
2024-03-08 15:33:11 +0100

    not needed anymore

--- casual/casual-event-server/src/main/java/se/laz/casual/event/server/EventServer.java
+++ casual/casual-event-server/src/main/java/se/laz/casual/event/server/EventServer.java
@@ -4,16 +4,10 @@ import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import se.laz.casual.api.flags.ErrorState;
-import se.laz.casual.event.Order;
 import se.laz.casual.event.ServiceCallEventHandler;
 import se.laz.casual.event.ServiceCallEventHandlerFactory;
-import se.laz.casual.event.ServiceCallEvent;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
@@ -64,50 +58,6 @@ public class EventServer
         channel.close().syncUninterruptibly();
         channel.eventLoop().shutdownGracefully().syncUninterruptibly();
         log.info(() -> "event server closed");
-    }
-
-    /*
-     * Test server that keeps posting test data to any potential clients, discards events if no clients
-     * Usage:
-     * telnet localhost port# ( default 7689)
-     * {"message":"HELLO"}
-     */
-    public static void main(String[] args)
-    {
-        List<String> arguments = Arrays.asList(args);
-        int port = arguments.stream()
-                            .map(value -> Integer.valueOf(value))
-                            .findFirst()
-                            .orElseGet(() -> 7689);
-        System.out.println("EventServer listening to port# " + port);
-        EventServerConnectionInformation connectionInformation = EventServerConnectionInformation.createBuilder()
-                                                                                                 .withPort(port)
-                                                                                                 .withUseEpoll(true)
-                                                                                                 .build();
-        EventServer.of(connectionInformation);
-        Executors.newSingleThreadExecutor().execute(() -> postTestData());
-    }
-
-    private static void postTestData()
-    {
-        while(true)
-        {
-            ServiceCallEventHandlerFactory.getHandler().put(ServiceCallEvent.createBuilder()
-                                                                            .withExecution(UUID.randomUUID())
-                                                                            .withCode(ErrorState.OK)
-                                                                            .withOrder(Order.SEQUENTIAL)
-                                                                            .withParent("Gigi")
-                                                                            .withExecution(UUID.randomUUID())
-                                                                            .build());
-            try
-            {
-                Thread.sleep(10 * 1000);
-            }
-            catch (InterruptedException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
 }
