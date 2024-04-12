9d5acf77cf82ec402515f072ff490be3cf1b5f44
cklellie <christopherjkelly@gmail.com>
2024-04-12 11:41:32 +0200

    Update to allow withStart and withEnd on event. Refactor time functions into InstantUtil.

--- casual/casual-api/src/test/groovy/se/laz/casual/api/util/time/InstantUtilTest.groovy
+++ casual/casual-api/src/test/groovy/se/laz/casual/api/util/time/InstantUtilTest.groovy
@@ -12,6 +12,8 @@ import spock.lang.Specification
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class InstantUtilTest extends Specification
 {
@@ -28,6 +30,22 @@ class InstantUtilTest extends Specification
         Instant instantRecovered = InstantUtil.fromNanos(nanos)
         then:
         instant == instantRecovered
+    }
+
+    def "Convert to Epoch Micro"()
+    {
+        when:
+        Instant date = ZonedDateTime.parse( input, DateTimeFormatter.ISO_ZONED_DATE_TIME).toInstant(  )
+        long actual = InstantUtil.toEpochMicro( date )
+
+        then:
+        actual == expected
+
+        where:
+        input                         || expected
+        "2024-04-12T12:34:56.123456Z" || 1712925296123456
+        "2024-04-12T12:34:56.123455Z" || 1712925296123455
+        "2024-04-12T12:34:56.124456Z" || 1712925296124456
     }
 
 }
