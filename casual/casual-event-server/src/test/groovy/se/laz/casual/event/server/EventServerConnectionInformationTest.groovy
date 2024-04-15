9db12b21e41b0a67032b20e681275b3c350d6d91
cklellie <christopherjkelly@gmail.com>
2024-04-15 10:08:20 +0200

    Fix copyright. Remove unused import.

--- casual/casual-event-server/src/test/groovy/se/laz/casual/event/server/EventServerConnectionInformationTest.groovy
+++ casual/casual-event-server/src/test/groovy/se/laz/casual/event/server/EventServerConnectionInformationTest.groovy
@@ -1,6 +1,11 @@
+/*
+ * Copyright (c) 2024, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
 package se.laz.casual.event.server
 
-import io.netty.channel.EventLoopGroup
 import io.netty.channel.epoll.EpollServerSocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import spock.lang.Specification
