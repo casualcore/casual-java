5f27075210e1d73fe1dc09e4d75c8af8010cd75b
Mathias Creutz <mathiascreutz@yahoo.se>
2024-04-24 16:27:37 +0200

    allow real, optional, method as well

--- casual/casual-inbound-handler-api/src/test/groovy/se/laz/casual/jca/inbound/handler/buffer/ServiceCallInfoTest.groovy
+++ casual/casual-inbound-handler-api/src/test/groovy/se/laz/casual/jca/inbound/handler/buffer/ServiceCallInfoTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2018, The casual project. All rights reserved.
+ * Copyright (c) 2017 - 2024, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
@@ -15,13 +15,15 @@ class ServiceCallInfoTest extends Specification
 {
     @Shared ServiceCallInfo instance
     @Shared Method method
+    @Shared Method realMethod
     @Shared Object[] objects
 
     def setup()
     {
+        realMethod = String.class.getMethod( "startsWith", String.class )
         method = String.class.getMethod( "split", String.class )
         objects = { "param" }
-        instance = ServiceCallInfo.of( method, objects )
+        instance = ServiceCallInfo.of( method, realMethod, objects )
     }
 
     def "get method"()
@@ -30,13 +32,20 @@ class ServiceCallInfoTest extends Specification
         instance.getMethod().get() == method
     }
 
-    def "get method null, return empty optional"()
+    def "get real method"()
+    {
+       expect:
+       instance.getRealMethod().get() == realMethod
+    }
+
+    def "get methods null, return empty optional"()
     {
         setup:
         instance = ServiceCallInfo.of( objects )
 
         expect:
         ! instance.getMethod().isPresent()
+        ! instance.getRealMethod().isPresent()
     }
 
     def "get params"()
