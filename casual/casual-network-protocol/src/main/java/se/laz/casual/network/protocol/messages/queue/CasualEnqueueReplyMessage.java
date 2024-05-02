72fa65c5487dc91f5759e88c2f07a340daf41c15
Mathias Creutz <mathiascreutz@yahoo.se>
2024-05-02 08:59:23 +0200

    Bugfix/3.2/log tool handle all messages (#111)
    * handle all message types and test that we do

--- casual/casual-network-protocol/src/main/java/se/laz/casual/network/protocol/messages/queue/CasualEnqueueReplyMessage.java
+++ casual/casual-network-protocol/src/main/java/se/laz/casual/network/protocol/messages/queue/CasualEnqueueReplyMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2018, The casual project. All rights reserved.
+ * Copyright (c) 2017 - 2024, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-public final class CasualEnqueueReplyMessage implements CasualNetworkTransmittable
+public class CasualEnqueueReplyMessage implements CasualNetworkTransmittable
 {
     private final UUID execution;
     private final UUID id;
