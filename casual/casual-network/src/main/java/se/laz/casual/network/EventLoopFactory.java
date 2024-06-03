ca8fcdec5d5b7896fea562e3d17490abd770f535
Mathias Creutz <mathiascreutz@yahoo.se>
2024-06-03 15:15:49 +0200

    outbound, reverse - share their own event loop groups

--- casual/casual-network/src/main/java/se/laz/casual/network/EventLoopFactory.java
+++ casual/casual-network/src/main/java/se/laz/casual/network/EventLoopFactory.java
@@ -1,29 +1,36 @@
 /*
- * Copyright (c) 2021, The casual project. All rights reserved.
+ * Copyright (c) 2021 - 2024, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
-package se.laz.casual.network.outbound;
+package se.laz.casual.network;
 
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import se.laz.casual.config.ConfigurationService;
 import se.laz.casual.config.Outbound;
+import se.laz.casual.network.outbound.JEEConcurrencyFactory;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 public final class EventLoopFactory
 {
     private static final Logger LOG = Logger.getLogger(EventLoopFactory.class.getName());
-    private static final EventLoopGroup INSTANCE = createEventLoopGroup();
+    private static final Map<EventLoopClient, EventLoopGroup> INSTANCES = new ConcurrentHashMap<>();
+    static
+    {
+        INSTANCES.put(EventLoopClient.OUTBOUND, createEventLoopGroup());
+        INSTANCES.put(EventLoopClient.REVERSE, createEventLoopGroup());
+    }
     private EventLoopFactory()
     {}
-    public static synchronized EventLoopGroup getInstance()
+    public static synchronized EventLoopGroup getInstance(EventLoopClient type)
     {
-        return INSTANCE;
+        return INSTANCES.get(type);
     }
-
     private static EventLoopGroup createEventLoopGroup()
     {
         Outbound outbound = ConfigurationService.getInstance().getConfiguration().getOutbound();
