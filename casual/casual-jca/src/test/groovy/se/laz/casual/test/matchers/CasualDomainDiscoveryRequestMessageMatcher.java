b7254565261008e2b3f8cd4d7802aea7563caf13
Mathias Creutz <mathiascreutz@yahoo.se>
2024-10-04 15:40:51 +0200

    latest sonar

--- casual/casual-jca/src/test/groovy/se/laz/casual/test/matchers/CasualDomainDiscoveryRequestMessageMatcher.java
+++ casual/casual-jca/src/test/groovy/se/laz/casual/test/matchers/CasualDomainDiscoveryRequestMessageMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2018, The casual project. All rights reserved.
+ * Copyright (c) 2017 - 2024, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
@@ -31,6 +31,7 @@ public final class CasualDomainDiscoveryRequestMessageMatcher
             @Override
             public void describeTo(Description description)
             {
+                description.appendText("servicenames and queuenames were not matching");
             }
 
             @Override
