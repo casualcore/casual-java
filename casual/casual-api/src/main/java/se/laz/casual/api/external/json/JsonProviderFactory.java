a40d71691d9be32f6d2c94ff9ab55402d6380ac8
Mathias Creutz <mathiascreutz@yahoo.se>
2024-12-05 13:45:13 +0100

    wip

--- casual/casual-api/src/main/java/se/laz/casual/api/external/json/JsonProviderFactory.java
+++ casual/casual-api/src/main/java/se/laz/casual/api/external/json/JsonProviderFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2017 - 2018, The casual project. All rights reserved.
+ * Copyright (c) 2017 - 2024, The casual project. All rights reserved.
  *
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
 
 package se.laz.casual.api.external.json;
 
-import se.laz.casual.api.external.json.impl.GsonProvider;
+import java.util.ServiceLoader;
 
 public final class JsonProviderFactory
 {
@@ -20,6 +20,7 @@ public final class JsonProviderFactory
      */
     public static JsonProvider getJsonProvider()
     {
-        return new GsonProvider();
+        ServiceLoader<JsonProvider> loader = ServiceLoader.load(JsonProvider.class);
+        return loader.iterator().next();
     }
 }
