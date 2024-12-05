f47aa0852814d8d3e02dd62f7c602d58995cef2c
Mathias Creutz <mathiascreutz@yahoo.se>
2024-12-05 16:21:30 +0100

    modularized

--- casual/casual-json-provider/src/main/java/se/laz/casual/impl/gson/GsonProvider.java
+++ casual/casual-json-provider/src/main/java/se/laz/casual/impl/gson/GsonProvider.java
@@ -4,7 +4,7 @@
  * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
  */
 
-package se.laz.casual.internal.spi;
+package se.laz.casual.impl.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
