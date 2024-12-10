9eb9ec3f5c06e0a51b2efc61f78d6e38c21ae2c1
cklellie <christopherjkelly@gmail.com>
2024-12-10 13:47:01 +0100

    Feature/3.2/initialise fieled on startup (#140)
    * Try to initialise fieled on startup if the user has provided a configuration for the file.

--- casual/casual-jca/src/main/java/se/laz/casual/jca/CasualResourceAdapter.java
+++ casual/casual-jca/src/main/java/se/laz/casual/jca/CasualResourceAdapter.java
@@ -19,6 +19,7 @@ import jakarta.resource.spi.work.Work;
 import jakarta.resource.spi.work.WorkException;
 import jakarta.resource.spi.work.WorkListener;
 import jakarta.resource.spi.work.WorkManager;
+import se.laz.casual.api.buffer.type.fielded.json.CasualFieldedLookup;
 import se.laz.casual.config.ConfigurationOptions;
 import se.laz.casual.config.ConfigurationService;
 import se.laz.casual.config.ReverseInbound;
@@ -83,7 +84,28 @@ public class CasualResourceAdapter implements ResourceAdapter, ReverseInboundLis
         //It is also not possible to inject with CDI on wildfly only ConfigProperty annotations.
 
         log.info( ConfigurationService.log() );
+        initialiseFielded();
         startEventServer();
+    }
+
+    /**
+     * Initialise fielded if there is a field table configured.
+     * This is to ensure json parsing required to populate static maps
+     * from within RA where `casual-api-spi-impl` is available.
+     * Therefore, other apps don't need to include casual-api-spi-impl in
+     * their own deployments too.
+     * Statics are unique per classloader.
+     * This only works due to the casual-api being available in a global module,
+     * meaning that the classloader is shared by all deployments.
+     * Any deployment utilising fielded is expected to have casual-jca deployed first.
+     */
+    private void initialiseFielded()
+    {
+        if( ConfigurationService.getConfiguration( ConfigurationOptions.CASUAL_FIELD_TABLE ) != null )
+        {
+
+            log.finest( ()-> "CasualFieldedLookup static initialisation: " + CasualFieldedLookup.getURL() );
+        }
     }
 
     private void startEventServer()
