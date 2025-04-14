336d4ce9bf7e595b22082e516965e3cb2778274d
Mathias Creutz <mathiascreutz@yahoo.se>
2025-04-14 13:57:09 +0200

    test

---
+++ casual/casual-api/src/test/groovy/se/laz/casual/network/ProtocolMatcherTest.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
+package se.laz.casual.network
+
+import spock.lang.Specification
+
+class ProtocolMatcherTest extends Specification
+{
+   def 'null'()
+   {
+      when:
+      ProtocolMatcher.match(null)
+      then:
+      thrown(NullPointerException)
+   }
+
+   def 'correctness'()
+   {
+      given:
+      def matchLowest = [ProtocolVersion.VERSION_1_0.version]
+      def matchHighest = [ProtocolVersion.VERSION_1_0.version, ProtocolVersion.VERSION_1_1.version, ProtocolVersion.VERSION_1_2.version]
+      when:
+      def lowest = ProtocolMatcher.match(matchLowest)
+      def highest = ProtocolMatcher.match(matchHighest)
+      then:
+      ProtocolVersion.VERSION_1_0 == ProtocolVersion.unmarshall(lowest)
+      ProtocolVersion.VERSION_1_2 == ProtocolVersion.unmarshall(highest)
+   }
+
+}
