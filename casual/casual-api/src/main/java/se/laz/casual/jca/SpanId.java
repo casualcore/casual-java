e1635bd53aa13fcd7f06967a9ce609d0d8df9bed
Mathias Creutz <mathiascreutz@yahoo.se>
2026-01-16 13:28:21 +0100

    SpanId test

--- casual/casual-api/src/main/java/se/laz/casual/jca/SpanId.java
+++ casual/casual-api/src/main/java/se/laz/casual/jca/SpanId.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
 package se.laz.casual.jca;
 
 import java.math.BigInteger;
---
+++ casual/casual-api/src/test/groovy/se/laz/casual/jca/SpanIdTest.groovy
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
+package se.laz.casual.jca
+
+import spock.lang.Specification
+
+class SpanIdTest extends Specification
+{
+   def 'sanity'()
+   {
+      when:
+      def spanId = SpanId.of(value)
+      def spanIdTwo = SpanId.of(value)
+      then:
+      spanId.getSpanId() == value
+      spanId.equals(spanIdTwo)
+      where:
+      value << (1L .. 100L)
+   }
+
+   def 'auto construction'()
+   {
+      when:
+      SpanId.of()
+      then:
+      noExceptionThrown()
+   }
+
+}
