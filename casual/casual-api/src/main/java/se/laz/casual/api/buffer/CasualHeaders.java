d755bb503825e22282d4f7fc762b687edce7663e
cklellie <christopherjkelly@gmail.com>
2026-04-30 14:22:13 +0200

    Wrapper type for casual headers.

---
+++ casual/casual-api/src/main/java/se/laz/casual/api/buffer/CasualHeaders.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2026, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
+package se.laz.casual.api.buffer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Headers associated with casual buffers.
+ * <br/>
+ * Headers are defined as name:value strings.
+ * <br/>
+ * Header names can occur multiple times.
+ * <br/>
+ * Therefore retrieval of a named header returns a {@link List} of values associated with the
+ * named header.
+ */
+public class CasualHeaders
+{
+    private static final String COLON_SEP = ":";
+
+    private final List<String> order;
+    private final Map<String,List<String>> entries;
+
+    private CasualHeaders( Builder builder )
+    {
+        this.order = builder.order;
+        this.entries = builder.entries;
+    }
+
+    /**
+     * Retrieve header by name.
+     *
+     * @param name of the header to retrieve.
+     * @return all values associated with the named header.
+     */
+    public List<String> get( String name )
+    {
+        Objects.requireNonNull( name, "Name is null." );
+        if( !entries.containsKey( name ) )
+        {
+            return Collections.emptyList();
+        }
+        return new ArrayList<>( entries.get( name ) );
+    }
+
+    /**
+     * Retrieve all header names present.
+     *
+     * @return unique list of headers present.
+     */
+    public Set<String> getNames( )
+    {
+        return new HashSet<>( entries.keySet() );
+    }
+
+    /**
+     * Retrieve all header name:value entries in insertion order.
+     *
+     * @return List of all name:value headers.
+     */
+    public List<String> getAll( )
+    {
+        List<String> all = new ArrayList<>(order.size());
+        Map<String,Integer> indexes = new HashMap<>();
+        for( String name: order )
+        {
+            int index = indexes.getOrDefault( name, 0 );
+            String value = entries.get( name ).get( index );
+            all.add( name + COLON_SEP + value );
+            indexes.put( name, ++index );
+        }
+        return all;
+    }
+
+    /**
+     * Number of headers stored.
+     * <br/>
+     * If a header with the same name is added multiple times,
+     * this will count each instance.
+     *
+     * @return number of headers.
+     */
+    public int size()
+    {
+        return this.order.size();
+    }
+
+    /**
+     * Check if a named header is present.
+     *
+     * @param name of the header to check.
+     * @return if present.
+     */
+    public boolean containsName( String name )
+    {
+        Objects.requireNonNull( name, "Name is null." );
+        return entries.containsKey( name );
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if( o == null || getClass() != o.getClass() )
+        {
+            return false;
+        }
+        CasualHeaders that = (CasualHeaders) o;
+        return Objects.equals( order, that.order ) && Objects.equals( entries, that.entries );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( order, entries );
+    }
+
+    @Override
+    public String toString()
+    {
+        return "CasualHeaders{" + "order=" + order +
+                ", entries=" + entries +
+                '}';
+    }
+
+    public static Builder newBuilder()
+    {
+        return new Builder();
+    }
+
+    public static Builder newBuilder( CasualHeaders src )
+    {
+        CasualHeaders.Builder b = CasualHeaders.newBuilder();
+
+        src.entries.forEach( ( key, value ) -> b.entries.put( key, new ArrayList<>( value ) ) );
+        b.order.addAll( src.order );
+
+        return b;
+    }
+
+    public static final class Builder
+    {
+        private final Map<String, List<String>> entries = new HashMap<>();
+        private final List<String> order = new ArrayList<>();
+
+        /**
+         * Add a name:value header entry.
+         *
+         * @param header the name:value header entry.
+         * @return the builder.
+         */
+        public Builder add( String header )
+        {
+            Objects.requireNonNull( header, "header is null." );
+            int index = header.indexOf( COLON_SEP );
+            if( index == -1 )
+            {
+                throw new IllegalArgumentException( "Header entry must contain a : " + header );
+            }
+            return this.add( header.substring( 0, index ), header.substring( index +1 ) );
+        }
+
+        /**
+         * Add a name, value header entry.
+         *
+         * @param name of the header.
+         * @param value of the header.
+         * @return the builder.
+         */
+        public Builder add( String name, String value )
+        {
+            Objects.requireNonNull( name, "Name is null." );
+            Objects.requireNonNull( value, "Value is null." );
+            order.add( name );
+            entries.computeIfAbsent( name, e -> new ArrayList<>() ).add( value );
+            return this;
+        }
+
+        /**
+         * Add a list of "name:value" header entries.
+         *
+         * @param headers to add.
+         * @return the builder.
+         */
+        public Builder addAll( List<String> headers )
+        {
+            Objects.requireNonNull( headers, "Headers is null." );
+            headers.forEach( this::add );
+            return this;
+        }
+
+        public CasualHeaders build()
+        {
+            return new CasualHeaders( this );
+        }
+    }
+}
---
+++ casual/casual-api/src/test/groovy/se/laz/casual/api/buffer/CasualHeadersTest.groovy
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2026, The casual project. All rights reserved.
+ *
+ * This software is licensed under the MIT license, https://opensource.org/licenses/MIT
+ */
+
+package se.laz.casual.api.buffer
+
+import spock.lang.Specification
+
+class CasualHeadersTest extends Specification
+{
+    CasualHeaders instance
+
+    def setup()
+    {
+        instance = CasualHeaders.newBuilder()
+                .add("a", "foo" )
+                .add("b", "bar" )
+                .add("c", "baz" )
+                .add("c", "baa" )
+                .build()
+    }
+
+    def "Create then get."()
+    {
+        expect:
+        instance.get( "a" ) == ["foo"]
+        instance.get( "b" ) == ["bar"]
+        instance.get( "c" ) == ["baz","baa"]
+        instance.size() == 4
+    }
+
+    def "Get headers does not exist, returns empty."()
+    {
+        when:
+        List<String> actual = instance.get( "d" )
+
+        then:
+        actual == Collections.emptyList(  )
+    }
+
+    def "Get with null throws NullPointerException."()
+    {
+        when:
+        instance.get( null )
+
+        then:
+        thrown NullPointerException
+    }
+
+    def "Add header invalid."()
+    {
+        when:
+        CasualHeaders.newBuilder()
+                .add( name, value )
+
+        then:
+        thrown NullPointerException
+
+        where:
+        name    | value
+        null    | "123"
+        "value" | null
+    }
+
+    def "Check if header is contained."()
+    {
+        when:
+        boolean actual = instance.containsName( name )
+
+        then:
+        actual == expected
+
+        where:
+        name | expected
+        "a" | true
+        "b" | true
+        "c" | true
+        "d" | false
+    }
+
+    def "Get header names"()
+    {
+        given:
+        Set<String> expected = ["a","b","c" ]
+
+        when:
+        Set<String> actual = instance.getNames( )
+
+        then:
+        actual == expected
+    }
+
+    def "Equals and hashcode."()
+    {
+        when:
+        CasualHeaders instance2 = CasualHeaders.newBuilder( instance ).build()
+        CasualHeaders instance3 = CasualHeaders.newBuilder( instance ).add( "other","something").build()
+
+        then:
+        instance == instance
+        instance.hashCode(  ) == instance.hashCode(  )
+        instance2 == instance
+        instance2.hashCode(  ) == instance.hashCode(  )
+        instance3 == instance3
+        instance3.hashCode(  ) == instance3.hashCode(  )
+        instance3 != instance
+        instance3.hashCode(  ) != instance.hashCode(  )
+
+        !instance.equals( "String" )
+    }
+
+    def "to string"()
+    {
+        when:
+        String actual = instance.toString()
+
+        then:
+        actual.contains( "a" )
+        actual.contains( "b" )
+        actual.contains( "c" )
+        actual.contains( "foo" )
+        actual.contains( "bar" )
+        actual.contains( "baz" )
+        actual.contains( "baa" )
+    }
+
+    def "Add fully."()
+    {
+        when:
+        CasualHeaders instance2 = CasualHeaders.newBuilder()
+                .add( "a:foo" )
+                .add( "b:bar" )
+                .add( "c:baz" )
+                .add( "c:baa" ).build()
+
+        then:
+        instance2 == instance
+    }
+
+    def "Add fully without right format invalid."()
+    {
+        when:
+        CasualHeaders.newBuilder( ).add( "me" )
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "Add same header multiple times, is ok."()
+    {
+        when:
+        instance = CasualHeaders.newBuilder().add( "me","here" ).add( "me","here" ).build()
+        List<String> actual = instance.get( "me" )
+
+        then:
+        instance.size() == 2
+        actual.size() == 2
+        actual == ["here","here"]
+    }
+
+    def "Add all headers from a list of colon delimited strings."()
+    {
+        List<String> all = ["a:foo", "b:bar", "c:baz", "c:baa" ]
+
+        when:
+        CasualHeaders instance2 = CasualHeaders.newBuilder().addAll( all ).build()
+
+        then:
+        instance2 == instance
+    }
+
+    def "Add all headers from a list of colon delimited strings, one invalid, throws IllegalArgumentException."()
+    {
+        List<String> all = ["a:foo", "b:bar", "cbaz", "c:baa" ]
+
+        when:
+        CasualHeaders.newBuilder().addAll( all )
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "Get all header entries."()
+    {
+        List<String> expected = [
+                "a:foo",
+                "b:bar",
+                "c:baz",
+                "c:baa"
+        ]
+        when:
+        List<String> actual = instance.getAll( )
+
+        then:
+        actual == expected
+    }
+
+}
